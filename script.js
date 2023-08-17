8b295b647a9f696f6fc6e14826d081bc332ccd58
Omkar Kendale <omkarkendale9@gmail.com>
2023-08-17 11:19:59 -0500

    Successful user entry and check vs sequenceArray added

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-squares = document.querySelectorAll(".gridSquare");
+//                                  Code for showing sequence of lights
+let metaObj = {
+    sequenceArray: [],
+    userEntryArray: [],
+    userEntryIndex: 0,
+}
 
+squares = document.querySelectorAll(".gridSquare");
 
 beginSequence = document.querySelector(".beginSequence");
 beginSequence.addEventListener('click', generateSquaresArray);
 
-
 function generateSquaresArray(){
-    squares[2].addEventListener('transitionend', function transition(e){
-        transitionToNextSquare(e, [2, 3, 5, 6, 3, 4, 8, 2, 1, 0, 4, 3], 0);
+    //Randomize the array generation
+    metaObj.sequenceArray = [2, 3, 5, 6];
+    squares[metaObj.sequenceArray[0]].addEventListener('transitionend', function transition(e){
+        transitionToNextSquare(e, metaObj.sequenceArray, 0);
     }, {once:true});
-    squares[2].classList.add("lightUp");
+    squares[metaObj.sequenceArray[0]].classList.add("lightUp");
     
 }
 
@@ -26,4 +33,26 @@ function transitionToNextSquare(e, arr, index){
         squares[arr[index+1]].classList.add("lightUp");
     }
     
+}
+
+
+//                                 Code for entering in sequence of lights
+
+enterButton = document.querySelector(".enterSequence");
+enterButton.addEventListener('click', () => {
+    squares.forEach(element => {
+        element.addEventListener('click', registerEntry);
+    });
+});
+
+function registerEntry(e){
+    if(parseInt(e.target.classList[0].charAt(4)) !== metaObj.sequenceArray[metaObj.userEntryIndex]){
+        alert("You lose!");
+    }
+    else{
+        if(metaObj.userEntryIndex == metaObj.sequenceArray.length - 1){
+            alert("You win!");
+        }
+        metaObj.userEntryIndex++;
+    }
 }
